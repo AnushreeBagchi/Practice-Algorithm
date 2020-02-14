0d3f393082e1b8c4dd10fd3df923453b7a6af17e
Anushree Bagchi <anushreebagchi4791@gmail.com>
2020-02-14 10:17:21 +0530

    quick-sort

---
+++ quick-sort.py
@@ -0,0 +1,30 @@
+def quick_sort(items):
+    sort_all(items, 0, len(items)-1)
+
+def sort_all(items, begin_index, end_index):
+    if begin_index > end_index:
+        return
+    pivot_index = sort_a_little(items, begin_index, end_index)
+    sort_all(items, begin_index, pivot_index-1)
+    sort_all(items, pivot_index+1, end_index)
+
+def sort_a_little(items, begin_index, end_index):
+    left_index = begin_index
+    pivot_index = end_index
+    pivot_value = items[pivot_index]
+    while left_index != pivot_index:
+        item = items[left_index]
+        if item < pivot_value:
+            left_index += 1
+            continue
+        else:
+            items[left_index]= items[pivot_index-1]
+            items[pivot_index-1] = items[pivot_index]
+            items[pivot_index] = item
+            pivot_index -=1
+    return pivot_index
+
+
+items = [8, 3, 1, 7, 0, 10, 2]
+quick_sort(items)
+print(items)
